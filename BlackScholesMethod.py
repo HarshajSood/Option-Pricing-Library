133041b8514f6f052d9afa3091393d2a0457fd7d
HarshajSood <harshajsood@gmail.com>
2024-07-23 13:45:24 +0100

    Add greeks Vanna and Volga, Add Dividend yield

--- BlackScholesMethod.py
+++ BlackScholesMethod.py
@@ -10,6 +10,7 @@ S - current price of underlying asset
 K - strike price of underlying asset
 T - time to maturity/expiry (in years)
 r - continuous compounding risk free interest rate
+q - dividend yield
 v - implied volatility
 
 N() - Normal Distribution
@@ -27,39 +28,37 @@ Gamma -  the rate of change in the delta with respect to changes in the underlyi
 Vanna -  the rate of change in the delta with respect to changes in the volatility
 Volga - the rate of change of volga with respect to the volatility (second order senstivity)
 """
-def d1(S,K,T,r,v):
-    return(log(S/K)+(r+v**2/2.)*T)/v*sqrt(T)
-def d2(S,K,T,r,v):
-    return d1(S,K,T,r,v)-v*sqrt(T)
+def d1(S,K,T,r,q,v):
+    return(log(S/K)+(r-q+v**2/2.)*T)/v*sqrt(T)
+def d2(S,K,T,r,q,v):
+    return d1(S,K,T,r,q,v)-v*sqrt(T)
 
-def blackScholesCall(S,K,T,r,v):
-    return S*norm.cdf(d1(S,K,T,r,v))-K*exp(-r*T)*norm.cdf(d2(S,K,T,r,v))
-def blackScholesPut(S,K,T,r,v):
-    return K*exp(-r*T)-S+bs_call(S,K,T,r,v)
+def blackScholesCall(S,K,T,r,q,v):
+    return exp(-q*T)*S*norm.cdf(d1(S,K,T,r,q,v))-K*exp(-r*T)*norm.cdf(d2(S,K,T,r,q,v))
+def blackScholesPut(S,K,T,r,q,v):
+    return K*exp(-r*T)*norm.cdf(-d2(S,K,T,r,q,v)) - exp(-q*T)*S*norm.cdf(-d1(S,K,T,r,q,v))
 
-## define the First Order Call Greeks
-def callDelta(S,K,T,r,v):
-    return norm.cdf(d1(S,K,T,r,v))
-def callVega(S,K,T,r,v):
-    return 0.01*(S*norm.pdf(d1(S,K,T,r,v))*sqrt(T))
-def callTheta(S,K,T,r,v):
-    return 0.01*(-(S*norm.pdf(d1(S,K,T,r,v))*v)/(2*sqrt(T)) - r*K*exp(-r*T)*norm.cdf(d2(S,K,T,r,v)))
-def callRho(S,K,T,r,v):
-    return 0.01*(K*T*exp(-r*T)*norm.cdf(d2(S,K,T,r,v)))
 
-## define Second Order Call Greeks
-def callGamma(S,K,T,r,v):
-    return norm.pdf(d1(S,K,T,r,v))/(S*v*sqrt(T))
+## First Order Greeks
+def callDelta(S,K,T,r,q,v):
+    return  exp(-q*T)*norm.cdf(d1(S,K,T,r,q,v))
+def putDelta(S,K,T,r,q,v):
+    return -exp(-q*T)*norm.cdf(-d1(S,K,T,r,q,v))
+def Vega(S,K,T,r,q,v):
+    return (S*norm.pdf(d1(S,K,T,r,q,v))*exp(-q*T)*sqrt(T))
+def callTheta(S,K,T,r,q,v):
+    return -(exp(-q*T)*S*norm.pdf(d1(S,K,T,r,q,v))*v)/(2*sqrt(T)) - r*K*exp(-r*T)*norm.cdf(d2(S,K,T,r,q,v))+q*exp(-q*T)*S*norm.cdf(d1(S,K,T,r,q,v))
+def putTheta(S,K,T,r,q,v):
+    return -(exp(-q*T)*S*norm.pdf(d1(S,K,T,r,q,v))*v)/(2*sqrt(T)) + r*K*exp(-r*T)*norm.cdf(-d2(S,K,T,r,q,v))-q*exp(-q*T)*S*norm.cdf(-d1(S,K,T,r,q,v))
+def callRho(S,K,T,r,q,v):
+    return K*T*exp(-r*T)*norm.cdf(d2(S,K,T,r,q,v))
+def putRho(S,K,T,r,q,v):
+    return -K*T*exp(-r*T)*norm.cdf(-d2(S,K,T,r,q,v))
 
-## Define First Order Put Greeks
-def putDelta(S,K,T,r,v):
-    return -norm.cdf(-d1(S,K,T,r,v))
-def putVega(S,K,T,r,v):
-    return 0.01*(S*norm.pdf(d1(S,K,T,r,v))*sqrt(T))
-def putTheta(S,K,T,r,v):
-    return 0.01*(-(S*norm.pdf(d1(S,K,T,r,v))*v)/(2*sqrt(T)) + r*K*exp(-r*T)*norm.cdf(-d2(S,K,T,r,v)))
-def putRho(S,K,T,r,v):
-    return 0.01*(-K*T*exp(-r*T)*norm.cdf(-d2(S,K,T,r,v)))
-## Define Second Order Put Greeks
-def putGamma(S,K,T,r,v):
-    return norm.pdf(d1(S,K,T,r,v))/(S*v*sqrt(T))
+## Second Order Greeks
+def Gamma(S,K,T,r,q,v):
+    return norm.pdf(d1(S,K,T,r,q,v))/(S*v*sqrt(T))
+def Vanna(S,K,T,r,q,v):
+    return -exp(-q*T)*norm.pdf(d1(S,K,T,r,q,v))*d2(S,K,T,r,q,v)/v
+def Volga(S,K,T,r,q,v):
+    return -exp(-q*T)*sqrt(T)*S*norm.pdf(d1(S,K,T,r,q,v))*d1*d2/v
